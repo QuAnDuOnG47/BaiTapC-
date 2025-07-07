012153fcfa232bca3021f54b96d884d5443bc3cb
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-07 21:23:57 +0700

    add bai tap

---
+++ Example2/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        float width, height;
+        Console.Write("Hay nhap gia tri chieu rong:");
+        width = Convert.ToSingle(Console.ReadLine());
+        Console.Write("Hay nhap gia tri chieu cao:");
+        height = Convert.ToSingle(Console.ReadLine());
+        float area = width * height;
+        Console.WriteLine($"Gia tri dien tich la : {area}");
+    }
+}
---
+++ ExampleCircle/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhan ban kinh hinh tron : ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        double area = Math.PI * radius * radius;
+        double pre = 2 * Math.PI * radius;
+        Console.WriteLine("Dien tich hinh tron la: " + area);
+        Console.WriteLine("Chu vi hinh tron la: " + pre);
+
+    }
+}
