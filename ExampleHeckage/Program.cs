d508d681cb60f9562c89986e3bfdb0b1501496dc
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-16 21:38:39 +0700

    Baitapc#

---
+++ ExampleHeckage/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap tuoi cua ban :");
+       int age  = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 18)
+        {
+            Console.WriteLine("Ban chua truong thanh ");
+        }
+        else if (age >= 18 && age < 25)
+        {
+            Console.WriteLine("Ban da du tuoi truong thanh");
+        }
+        else
+        {
+            Console.WriteLine("Ban da qua tuoi truong thanh");
+        }
+        
+        Console.ReadKey();
+    }
+}
