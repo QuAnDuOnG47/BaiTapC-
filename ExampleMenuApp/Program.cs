b10e63b8d26f4c32580829ad5dd02b27a4b3a37e
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 05:32:38 +0700

    update featrue

--- ExampleMenuApp/Program.cs
+++ ExampleMenuApp/Program.cs
@@ -2,7 +2,12 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hay nhta vao 1 so : ");
+        Console.WriteLine("Hay nhap vao 1 so : ");
+        Console.WriteLine("1. Hien thi hinh tam giac sao"); 
+        Console.WriteLine("2. Hien thi hinh chu nhat sao");
+        Console.WriteLine("3. Hien thi hinh vuong sao");
+        Console.WriteLine("4. Hien thi hinh tron sao");
+        Console.WriteLine("0. Thoat chuong trinh");
         int choice = -1;
         while (choice != 0)
         {
@@ -31,26 +36,25 @@
                     Console.WriteLine("********");
                     break;
                     case 4:
-    int radius = 5;
-    double thickness = 0.4;
-    char symbol = '*';
+                    int radius = 5;
+                    double thickness = 0.4;
+                    char symbol = '*';
 
-    double rIn = radius - thickness, rOut = radius + thickness;
-    for (double y = radius; y >= -radius; --y)
-    {
-        for (double x = -radius; x < rOut; x += 0.5)
-        {
-            double value = x * x + y * y;
-            if (value >= rIn * rIn && value <= rOut * rOut)
-                Console.Write(symbol);
-            else
-                Console.Write(' ');
-        }
-        Console.WriteLine();
-    }
-    break;
+                    double rIn = radius - thickness, rOut = radius + thickness;
+                    for (double y = radius; y >= -radius; --y)
+                    {
+                        for (double x = -radius; x < rOut; x += 0.5)
+                        {
+                            double value = x * x + y * y;
+                            if (value >= rIn * rIn && value <= rOut * rOut)
+                                Console.Write(symbol);
+                            else
+                                Console.Write(' ');
+                        }
+                        Console.WriteLine();
+                    }
                     break;
-                case 0:
+                    case 0:
                     Environment.Exit(0);
                     break;
                 default:
