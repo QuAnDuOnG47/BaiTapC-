384cf34c3c084c203592ad039f84f752df411c59
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 04:24:28 +0700

    add Example

---
+++ Example_Show_Name/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap ten cua ban:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Ten cua ban la: " + name);
+    }
+}
