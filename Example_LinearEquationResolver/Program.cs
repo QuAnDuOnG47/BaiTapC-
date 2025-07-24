035c15150da4d3d785c775028ee4f3cbbf071f82
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 04:40:09 +0700

    add example phuong trinh bac nhat

---
+++ Example_LinearEquationResolver/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.WriteLine("Giai phuong trinh bac nhat ax + b = c");
+        Console.WriteLine("Nhap cac he so a, b, c:");
+        Console.Write("Nhap a:");
+        float a = float.Parse(Console.ReadLine());
+        Console.Write("Nhap b:");
+        float b = float.Parse(Console.ReadLine());
+        Console.Write("Nhap c:");
+        float c = float.Parse(Console.ReadLine());
+
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Phuong trinh vo so nghiem");
+            }
+            else
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+        }
+        else
+        {
+            float x = (c - b) / a;
+            Console.WriteLine("Nghiem cua phuong trinh la: " + x);
+        }
+    }
+}
