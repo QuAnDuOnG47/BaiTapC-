b5660cb6092ff453b8602f0c6adfb95ff1e7acd2
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 04:45:49 +0700

    add example BMI

---
+++ Example_Body_BMI/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap chieu cao (m):");
+        float height = float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap can nang (kg):");
+        float weight = float.Parse(Console.ReadLine());
+
+        float bmi = weight / (height * height);
+        if(bmi < 18.5)
+        {
+            Console.WriteLine("Chi so BMI la: " + bmi + " Ban bi thieu can.");
+        }
+        else if(bmi < 25.0)
+        {
+            Console.WriteLine("Chi so BMI la: " + bmi + " Ban co can nang binh thuong.");
+        }
+        else if(bmi < 30.0)
+        {
+            Console.WriteLine("Chi so BMI la: " + bmi + " Ban bi thua can.");
+        }
+        else
+        {
+            Console.WriteLine("Chi so BMI la: " + bmi + " Ban bi beo phi.");
+        }
+       
+    }
+}
