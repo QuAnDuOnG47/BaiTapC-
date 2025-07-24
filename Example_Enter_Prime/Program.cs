e04354725957c03c7b441247dca360fa04c97d6f
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 05:37:35 +0700

    add example Prime

---
+++ Example_Enter_Prime/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so bat ky: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (IsPrime(number))
+        {
+            Console.WriteLine(number + " la so nguyen to.");
+        }
+        else
+        {
+            Console.WriteLine(number + " khong la so nguyen to.");
+        }
+    }
+
+    public static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+        for (var i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
