2e290f9717ccf10eef3c1583a724c56566655804
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 04:54:13 +0700

    add example leap year

---
+++ Example_Leap_Year/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap nam:");
+        int year = int.Parse(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine(year + " la nam nhuan.");
+        }
+        else
+        {
+            Console.WriteLine(year + " khong la nam nhuan.");
+        }
+    }
+}
