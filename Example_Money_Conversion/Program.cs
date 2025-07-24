ab105bf9f8baa7e8f7ae0b4fa4ec37e4d4dcdb69
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 04:31:11 +0700

    add Example Money

---
+++ Example_Money_Conversion/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so tien USD:");
+        float usd = float.Parse(Console.ReadLine());
+        float vnd = usd * 23000;
+        Console.WriteLine("Gia tri VND la: " + vnd + " VND");
+    }
+}
