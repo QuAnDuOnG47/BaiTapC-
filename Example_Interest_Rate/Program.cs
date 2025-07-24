5ea6581d5fa789294a0efd2a7558ce1132c0ccb1
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 05:24:15 +0700

    add example interest

---
+++ Example_Interest_Rate/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so tien gui ngan hang: ");
+        double principal = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhap lai suat (%): ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhap so thang gui: ");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        double totalInterset = 0;
+        for (int i = 0; i < month; i++)
+        {
+            totalInterset += principal * (rate / 100) / 12;
+        }
+        double totalAmount = principal + totalInterset;
+        Console.WriteLine("So tien lai nhan duoc sau " + months + " thang la: " + totalInterset);
+        Console.WriteLine("Tong so tien sau " + months + " thang la: " + totalAmount);
+    }
+}
