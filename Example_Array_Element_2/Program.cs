28eabe451de383a292250e4a9f8d5d6bbc6cec61
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-26 06:01:36 +0700

    Add example array

---
+++ Example_Array_Element_2/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so cot mang 2 chieu: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap so hang mang 2 chieu: ");
+        int m = int.Parse(Console.ReadLine());
+        int[,] arr = new int[n, m];
+
+        Console.WriteLine("Nhap cac phan tu cua mang: ");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write($"arr[{i},{j}]: ");
+                arr[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("Mang da nhap la: ");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(arr[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        int max = arr[0, 0];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (arr[i, j] > max)
+                {
+                    max = arr[i, j];
+                }
+            }
+        }
+        Console.WriteLine($"Phan tu lon nhat trong mang la: {max}");
+    }
+}
