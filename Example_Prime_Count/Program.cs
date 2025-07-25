f402d5b1cc699a4bc08d8045f9f57cc7c3ca91d7
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 17:53:51 +0700

    add example prime count

---
+++ Example_Prime_Count/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so luong so nguyen to muon in :");
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+        int num = 2;
+        while (count < n)
+        {
+            if (IsPrime(num))
+            {
+                
+                Console.WriteLine("So thu " + (count + 1) + " la: " + num);
+                count++;
+            }
+            num++;
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
