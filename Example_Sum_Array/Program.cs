607eb51178726df6c9da4f3632884609f9e98886
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 18:02:17 +0700

    add example sum array

---
+++ Example_Sum_Array/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine("Cac so trong mang la:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+        Console.WriteLine("Tong cac so trong mang la: " + sum);
+    }
+}
