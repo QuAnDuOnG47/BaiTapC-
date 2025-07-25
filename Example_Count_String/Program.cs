674b9fc448363b9e598a31e6fe4ba115bf2bc608
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-26 06:36:14 +0700

    add example

---
+++ Example_Count_String/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap chuoi ky tu :");
+        string input = Console.ReadLine();
+        Console.WriteLine("Nhap ky tu can dem:");
+        char characterToCount = Console.ReadKey().KeyChar;
+
+        int count = CountCharacters(input, characterToCount);
+        Console.WriteLine(); // Xuống dòng sau khi nhập ký tự
+        Console.WriteLine($"So luong ky tu '{characterToCount}' trong chuoi la: {count}");
+    }
+
+    static int CountCharacters(string str, char character)
+    {
+        int count = 0;
+        for(int i = 0; i < str.Length; i++)
+        {
+            if(str[i] == character)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
