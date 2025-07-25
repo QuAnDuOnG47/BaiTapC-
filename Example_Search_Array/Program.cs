8c1924e524d23b2eaa49b44c310d8740ee4e64d7
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 18:10:53 +0700

    add example search array

---
+++ Example_Search_Array/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] students = { "Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily" };
+        Console.WriteLine("Nhap ten sinh vien muon tim :");
+        string name = Console.ReadLine();
+
+        bool found = false;
+        for (int i = 0; i < students.Length; i++)
+        {
+            if (students[i].Equals(name))
+            {
+                Console.WriteLine("Vi tri cua sinh vien " + name + " la : " + (i + 1) + " trong danh sach");
+                found = true;
+                break;
+            }
+        }
+
+        if (found != true)
+        {
+            Console.WriteLine("Sinh vien " + name + " khong ton tai trong danh sach.");
+        }
+       
+        
+
+    }
+
+    }
+
