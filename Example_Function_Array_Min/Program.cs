00673ad6a097965b35c39ccd5112600ab9f92cc1
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-26 06:17:18 +0700

    add example function min array7

---
+++ Example_Function_Array_Min/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhập số phần tử của mảng:");
+        int a = int.Parse(Console.ReadLine());
+        int[] arr = new int[a];
+        Console.WriteLine("Nhập các phần tử của mảng:");
+        for (int i = 0; i < a; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        int min = FindMin(arr);
+        Console.WriteLine($"Giá trị nhỏ nhất trong mảng là: {min}");
+    }
+
+    static int FindMin(int[] arr)
+    {
+        int min = arr[0];
+        foreach (int num in arr)
+        {
+            if (num < min)
+            {
+                min = num;
+            }
+        }
+        return min;
+    }
+}
