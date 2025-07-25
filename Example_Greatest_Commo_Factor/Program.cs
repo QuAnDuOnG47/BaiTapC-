eac5ec1b4adff1eb1bdfd93166348839c4f5fb88
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-07-25 17:39:53 +0700

    addd example grearest comom factor

---
+++ Example_Greatest_Commo_Factor/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Nhap so b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int gcd = FindGCD(a, b);
+        Console.WriteLine($"UCLN cua {a} va {b} la: {gcd}");
+    }
+
+    static int FindGCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
