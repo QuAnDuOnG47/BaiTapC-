488019ecfcb0f0ae0367cc29608e0b7cdc723fde
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-08-03 16:57:03 +0700

    add example static method

---
+++ Example_Static_Method/Program.cs
@@ -0,0 +1,36 @@
+﻿class Student
+{
+    int id;
+    string name;
+    static string college = "ABC University";
+    public  void Display()
+    {
+        Console.WriteLine(id + " " + name + " " + college);
+    }
+    public Student(int i, string n)
+    {
+        id = i;
+        name = n;
+    }
+    public static void Change()
+    {
+        college = "Code Gym";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+       Student.Change();
+                             //creating objects
+            Student s1 = new Student(111, "Hoang");
+            Student s2 = new Student(222, "Khanh");
+            Student s3 = new Student(333, "Nam");
+            
+            s1.Display();
+            s2.Display();
+            s3.Display();
+    }
+}
