2c890bb3a94b51a33264f17ea13cfce93a53a9ce
QuAnDuOnG47 <141488554+QuAnDuOnG47@users.noreply.github.com>
2025-08-19 22:59:13 +0700

    Add Example

---
+++ ExamplePointClass/Program.cs
@@ -0,0 +1,52 @@
+﻿public class Point2D
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Point2D(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Point2D()
+    {
+
+    }
+    public override string ToString()
+    {
+        return $"Point2D({X}, {Y})";
+    }
+}
+
+public class Point3D : Point2D
+{
+    public double Z { get; set; }
+
+    public Point3D(double x, double y, double z) : base(x, y)
+    {
+        Z = z;
+    }
+
+    public Point3D() : base()
+    {
+
+    }
+
+    public override string ToString()
+    {
+        return $"Point3D({X}, {Y}, {Z})";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point2D point2D = new Point2D(3.0, 4.0);
+        Console.WriteLine(point2D);
+
+        Point3D point3D = new Point3D(3.0, 4.0, 5.0);
+        Console.WriteLine(point3D);
+    }
+}
